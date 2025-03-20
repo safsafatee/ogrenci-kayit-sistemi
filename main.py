28443e03bea2e0fd467cda4d4059273b018a7210
SAFA AL-GALEDI <139810843+safsafatee@users.noreply.github.com>
2025-03-20 15:10:33 +0300

    .

--- main.py
+++ main.py
@@ -142,3 +142,145 @@ def temizle():
     ui.comboBox_Filitre.setCurrentIndex(-1)
 
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+                kayitlar=veri.fetchone()
+                print(kayitlar[0])
+                finalnotu=kayitlar["Final"]
+                print(finalnotu)
+
+"""
+
+
+
+
+
+def tumkayitlar():
+    listele()
+
+def guncelle():
+    
+ 
+ 
+    Ad=ui.lineEdit_Ad.text()
+    Soyad=ui.lineEdit_Soyad.text()
+    Ders=ui.comboBox_Ders.currentText()
+    Vize=ui.lineEdit_Vize.text()
+    Final=ui.lineEdit_Final.text()
+    numara_kontrol=ui.lineEdit_Numara.text()
+    """
+
+    
+    Ortalama=int((Vize*0.4)+(Final*0.6))
+    Harf=""
+    Durum=""
+    if Ortalama<50:
+        Harf="FF"
+    elif Ortalama>=50 and Ortalama<60:
+        Harf="DD"
+    elif Ortalama>=60 and Ortalama<70 :
+        Harf="CC"
+    elif Ortalama>=70 and Ortalama<=80:
+        Harf="BB"
+    elif Ortalama>80:
+        Harf="AA"
+    else:
+        ui.statusbar.showMessage("HAYIR",5000)
+        return
+    if Ortalama>=50 :
+        Durum="GEÇTİ"
+    else:
+        Durum="KALDI"
+     """   
+    guncelleme_onay=QMessageBox.question(pencere,"GÜNCELLEME","Kaydı güncellemek istediğinizden emin misiniz?",QMessageBox.Yes|QMessageBox.No)    
+    if guncelleme_onay==QMessageBox.StandardButton.Yes:
+        print(Numara)
+        try:
+            if Soyad=="" and Ders=="" and Vize=="" and Final=="":
+                Numara=int(ui.lineEdit_Numara.text())
+                sorgu="Update ogrenci Ad= ? where Numara=?"
+                islem.execute(sorgu,(Ad,Numara))
+            elif Ad=="" and Ders=="" and Vize=="" and Final=="":
+                Numara=int(ui.lineEdit_Numara.text())
+                sorgu="update ogrenci set Soyad= ?  where Numara=?"
+                islem.execute(sorgu,(Soyad,Numara))
+            elif Ad=="" and Soyad=="" and Vize=="" and Final=="":
+                sorgu="update ogrenci set Ders= ?  where Numara=?"
+                islem.execute(sorgu,(Ders,Numara)) 
+            elif Ad=="" and Soyad=="" and Ders=="" and Final=="":
+                print("Vize")
+                vize_al=int(ui.lineEdit_Vize.text())
+                final_sorgu="select Final from ogrenci where Numara= ?"
+                print(final_sorgu)
+                veri=islem.execute(final_sorgu,(Numara,))
+        
+                sorgu="update ogrenci set Vize= ?  where Numara=?"
+                islem.execute(sorgu,(Ders,vize_al)) 
+            elif Ad=="" and Soyad=="" and Ders=="" and Vize=="":
+                final_al=int(ui.lineEdit_Final.text())
+                sorgu="update ogrenci set Final= ?  where Numara=?"
+                islem.execute(sorgu,(Ders,vize_al)) 
+            elif Ad!="" and Soyad!="" and Ders!="" and Vize!="" and Final!="":
+                final_al=int(ui.lineEdit_Final.text())
+                vize_al=int(ui.lineEdit_Vize.text())
+                Ortalama=int((vize_al*0.4)+(final_al*0.6))
+                Harf=""
+                Durum=""
+                if Ortalama<50:
+                    Harf="FF"
+                elif Ortalama>=50 and Ortalama<60:
+                    Harf="DD"
+                elif Ortalama>=60 and Ortalama<70 :
+                    Harf="CC"
+                elif Ortalama>=70 and Ortalama<=80:
+                    Harf="BB"
+                elif Ortalama>80:
+                    Harf="AA"
+                else:
+                    ui.statusbar.showMessage("HAYIR",5000)
+                    return
+                if Ortalama>=50 :
+                    Durum="GEÇTİ"
+                else:
+                    Durum="KALDI"
+                sorgu="update ogrenci set Final= ?  where Numara=?"
+                islem.execute(sorgu,(Ders,vize_al))                          
+            baglanti.commit()
+            listele()
+        except:
+            pass
+
+
+
+
+
+
+ui.pushButton_Ekle.clicked.connect(ekle)
+ui.pushButton_Sil.clicked.connect(sil)
+ui.pushButton_Filitrele.clicked.connect(filitrele)
+ui.pushButton_tumKayitlar.clicked.connect(tumkayitlar)
+ui.pushButton_Ekle_Guncelle.clicked.connect(guncelle)
+ui.pushButton_Ekle_Temizle.clicked.connect(temizle)
+listele()
+
+sys.exit(uygulama.exec_())
+
+
+
+
+
+
+
